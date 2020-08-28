ad8ce0fc995983211500ef1f07af570b25fe95d9
R _ R <exit19093@gmail.com>
2020-08-28 14:01:36 +0900

    init

--- platform/src/index.js
+++ platform/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
   document.getElementById('root')
 );
+
+// if you want your app to work offline and load faster, unregister -> register
+// PWA service
+serviceWorker.unregister();
