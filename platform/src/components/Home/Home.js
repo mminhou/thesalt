6519f71aae98aa2299e572c163f31a5a95a7fd47
R _ R <exit19093@gmail.com>
2021-02-17 14:23:25 +0900

    Feat: Home component 추가

--- platform/src/components/Home/Home.js
+++ platform/src/components/Home/Home.js
@@ -9,25 +9,25 @@ const Home = () => {
     return (
         <div style={{backgroundColor: 'rgb(235, 234, 229)'}}>
             <Grid container style={{textAlign: "center"}}>
-              <Grid item xs={12}>
-                  <div style={{borderBottom: '1px solid black', height: 500, marginBottom: 20}}>
-                      <HomeCarousel />
-                  </div>
-              </Grid>
-              <Grid item xs={12} style={{  height: '100%' }}>
-                  New pagination 6 * 3
-                  <HomePagination />
-              </Grid>
-              <Grid item xs={12} style={{ height: 150 }}>
+                <Grid item xs={12}>
+                    <div style={{borderBottom: '1px solid black', height: 500, marginBottom: 20}}>
+                        <HomeCarousel />
+                    </div>
+                </Grid>
+                <Grid item xs={12} style={{  height: '100%' }}>
+                    New pagination 6 * 3
+                    <HomePagination />
+                </Grid>
+                <Grid item xs={12} style={{ height: 150 }}>
 
 
-                  open project -> link page(create board)
-              </Grid>
-              <Grid item xs={12}>
-                  <Footer />
-              </Grid>
-          {/* mainImage, title, funding, name */}
-          </Grid>
+                    open project -> link page(create board)
+                </Grid>
+                <Grid item xs={12}>
+                    <Footer />
+                </Grid>
+                {/* mainImage, title, funding, name */}
+            </Grid>
 
         </div>
     );
