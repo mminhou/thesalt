286db3c8704daecebef1415d739a24949bdcfb31
R _ R <exit19093@gmail.com>
2021-02-17 15:10:24 +0900

    Fix: Component Rerendering some fixed

--- platform/src/components/Nav/Nav.js
+++ platform/src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import PropTypes from 'prop-types';
 import { AppBar, Toolbar, Typography, List, ListItem, withStyles, Grid, SwipeableDrawer} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -22,11 +22,11 @@ const styleSheet = {
     letterSpacing: 2,
     width: '100px',
     textAlign: "center",
-     "&:hover": {
+    "&:hover": {
       border: '1px solid white',
-       borderRadius: 10
+      borderRadius: 10
     },
-     "&:active": {
+    "&:active": {
       border: '1px solid white',
     },
   },
@@ -41,7 +41,7 @@ class Nav extends Component{
   constructor(props){
     super(props);
     this.state = {drawerActivate:false, drawer:false, color: 'transparent', height: '13%', imgHeight: 80,
-          isModalOpen: false, isLoggedIn: false,};
+      isModalOpen: false, isLoggedIn: false,};
     this.createDrawer = this.createDrawer.bind(this);
     this.destroyDrawer = this.destroyDrawer.bind(this);
 
@@ -73,43 +73,43 @@ class Nav extends Component{
   //Small Screens
   createDrawer(){
     return (
-      <div>
-        <AppBar style={{backgroundColor: 'rgb(25,25,25)'}}>
-          <Toolbar>
-            <Grid container direction = "row" justify = "space-between" alignItems="center">
-              <MenuIcon
-                className = {this.props.classes.sideBarIcon}
-                onClick={()=>{this.setState({drawer:true})}} />
+        <div>
+          <AppBar style={{backgroundColor: 'rgb(25,25,25)'}}>
+            <Toolbar>
+              <Grid container direction = "row" justify = "space-between" alignItems="center">
+                <MenuIcon
+                    className = {this.props.classes.sideBarIcon}
+                    onClick={()=>{this.setState({drawer:true})}} />
 
-              <Typography color="inherit" variant = "headline">
-                <img src={logo} height={50}></img>
-              </Typography>
-              <Typography color="inherit" variant = "headline"></Typography>
-            </Grid>
-          </Toolbar>
-        </AppBar>
+                <Typography color="inherit" variant = "headline">
+                  <img src={logo} height={50}></img>
+                </Typography>
+                <Typography color="inherit" variant = "headline"></Typography>
+              </Grid>
+            </Toolbar>
+          </AppBar>
 
-        <SwipeableDrawer
-         open={this.state.drawer}
-         onClose={()=>{this.setState({drawer:false})}}
-         onOpen={()=>{this.setState({drawer:true})}}>
+          <SwipeableDrawer
+              open={this.state.drawer}
+              onClose={()=>{this.setState({drawer:false})}}
+              onOpen={()=>{this.setState({drawer:true})}}>
 
-           <div
-             tabIndex={0}
-             role="button"
-             onClick={()=>{this.setState({drawer:false})}}
-             onKeyDown={()=>{this.setState({drawer:false})}}>
+            <div
+                tabIndex={0}
+                role="button"
+                onClick={()=>{this.setState({drawer:false})}}
+                onKeyDown={()=>{this.setState({drawer:false})}}>
 
-            <List className = {this.props.classes.list}>
-               <ListItem key = {1} button divider> Option 1 </ListItem>
-               <ListItem key = {2} button divider> Option 2 </ListItem>
-               <ListItem key = {3} button divider> Option 3 </ListItem>
-             </List>
+              <List className = {this.props.classes.list}>
+                <ListItem key = {1} button divider> Option 1 </ListItem>
+                <ListItem key = {2} button divider> Option 2 </ListItem>
+                <ListItem key = {3} button divider> Option 3 </ListItem>
+              </List>
 
-         </div>
-       </SwipeableDrawer>
+            </div>
+          </SwipeableDrawer>
 
-      </div>
+        </div>
     );
   }
 
@@ -117,45 +117,46 @@ class Nav extends Component{
   destroyDrawer(){
     const {classes} = this.props
     const pathname = window.location.pathname
+    // const [dummy, reload] = useState(false);
 
     return (
-      <AppBar style={{ WebkitTransition: 'all 0.5s ease-in-out', transition: 'all 0.5s ease-in-out',
-        backgroundColor: this.state.color , height: this.state.height, boxShadow: 'none', display: 'flex',
-    justifyContent: 'center', marginTop: 10}}>
-        <Toolbar>
-          <Typography style={{flexGrow:1}}>
-            <Link to="/"  onClick={() => window.location.reload()}>
-              <img onClick src={logo} height={this.state.imgHeight} style={{WebkitTransition: 'all 0.7s ease-in-out', transition: 'all 0.7s ease-in-out', verticalAlign: "middle"}}></img>
-            </Link>
-          </Typography>
+        <AppBar style={{ WebkitTransition: 'all 0.5s ease-in-out', transition: 'all 0.5s ease-in-out',
+          backgroundColor: this.state.color , height: this.state.height, boxShadow: 'none', display: 'flex',
+          justifyContent: 'center', marginTop: 10}}>
+          <Toolbar>
+            <Typography style={{flexGrow:1}}>
+              <a href="/" >
+                <img src={logo}  height={this.state.imgHeight} style={{WebkitTransition: 'all 0.7s ease-in-out', transition: 'all 0.7s ease-in-out', verticalAlign: "middle"}}></img>
+              </a>
+            </Typography>
 
-          <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
-            <Link to="/home" className={classes.navLink} activeClassName="selected" >home</Link>
-          </Typography>
-          <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
-            <Link to="/funding" className={classes.navLink} activeClassName={classes.selected}>funding</Link>
-          </Typography>
-          <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
-            <Link to="/about" className={classes.navLink}>about</Link>
-          </Typography>
-          <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
-            <Link to="/contact" className={classes.navLink}>contact</Link>
-          </Typography>
-          <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/'? 'none': null }}>
-            { this.state.isLoggedIn ?
-                (<Link to="/" className={classes.navLink} onClick={this.state.isLoggedIn = false}>logout</Link> )
-                :
-                (<div>
-                  <Link to="/login" className={classes.navLink} onClick={() => window.location.reload()}
+            <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
+              <Link to="/home" className={classes.navLink} activeClassName="selected" >home</Link>
+            </Typography>
+            <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
+              <Link to="/funding" className={classes.navLink} activeClassName={classes.selected}>funding</Link>
+            </Typography>
+            <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
+              <Link to="/about" className={classes.navLink}>about</Link>
+            </Typography>
+            <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/' || pathname == '/login' ? 'none': null }}>
+              <Link to="/contact" className={classes.navLink}>contact</Link>
+            </Typography>
+            <Typography variant = "" className = {classes.padding} color="inherit" style={{ display: pathname == '/'? 'none': null }}>
+              { this.state.isLoggedIn ?
+                  (<Link to="/" className={classes.navLink} onClick={this.state.isLoggedIn = false}>logout</Link> )
+                  :
+                  (<div>
+                    <Link to="/login" className={classes.navLink} onClick={() => window.location.reload()}
                         // onClick={this.openModal}
-                  ><div onClick>login</div></Link>
-                  {/*<SignIn isOpen={this.state.isModalOpen} close={this.closeModal} />*/}
-                </div>)
-            }
-          </Typography>
+                    ><div onClick>login</div></Link>
+                    {/*<SignIn isOpen={this.state.isModalOpen} close={this.closeModal} />*/}
+                  </div>)
+              }
+            </Typography>
 
-        </Toolbar>
-      </AppBar>
+          </Toolbar>
+        </AppBar>
 
     )
   }
@@ -174,9 +175,9 @@ class Nav extends Component{
 
   render(){
     return(
-      <div>
-        {this.state.drawerActivate ? this.createDrawer() : this.destroyDrawer()}
-      </div>
+        <div>
+          {this.state.drawerActivate ? this.createDrawer() : this.destroyDrawer()}
+        </div>
     );
   }
 }
