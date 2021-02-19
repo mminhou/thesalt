7200a238e9600118b543100292b14d7421149187
R _ R <exit19093@gmail.com>
2021-02-19 13:38:41 +0900

    Feat: seperate type from action

--- platform/src/modules/actions/productAction.js
+++ platform/src/modules/actions/productAction.js
@@ -1,19 +1,23 @@
+export const LOAD_PRODUCT = "LOAD_PRODUCT";
+export const LOAD_PRODUCT_SUCCESS = "LOAD_PRODUCT_SUCCESS";
+export const LOAD_PRODUCT_FAIL = "LOAD_PRODUCT_FAL";
+
 export const loadProduct = () => {
     return{
-        type: "LOAD_PRODUCT"
+        type: LOAD_PRODUCT
     };
 };
 
 export const loadProductSuccess = products => {
     return{
-        type: "LOAD_PRODUCT_SUCCESS",
+        type: LOAD_PRODUCT_SUCCESS,
         products: products
     };
 };
 
 export const loadProductFail = error => {
     return{
-        type: "LOAD_PRODUCT_FAIL",
+        type: LOAD_PRODUCT_FAIL,
         error
     };
 };
