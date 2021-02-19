51bc39bec1c9cac373acd10f2f7d7ae7c944dba6
R _ R <exit19093@gmail.com>
2021-02-19 15:08:49 +0900

    Feat: add loadedproduct action to reducer

--- platform/src/modules/reducers/productReducer.js
+++ platform/src/modules/reducers/productReducer.js
@@ -1,9 +1,16 @@
+import { LOAD_PRODUCT, LOAD_PRODUCT_FAIL, LOAD_PRODUCT_SUCCESS } from '../actions/productAction'
+
 const products = (state = [], action) => {
     switch(action.type){
-        case "LOAD_PRODUCT_SUCCESS":
+        case LOAD_PRODUCT_SUCCESS:
             return [...state, ...action.products];
-        case "LOAD_PRODUCT_FAIL":
+        case LOAD_PRODUCT_FAIL:
             return [...state, action.error];
+        case "LOADED_PRODUCT":
+            return {
+                status: "complete",
+                products: action.products,
+            };
         default:
             return state;
     }
