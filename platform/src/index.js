a7efe33bba59064b398df1927066a3e8156f303e
R _ R <exit19093@gmail.com>
2021-02-19 16:54:10 +0900

    Feat: redux-persist module 추가

--- platform/src/index.js
+++ platform/src/index.js
@@ -6,10 +6,12 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from "./modules/reducers";
 // Provider
 import { Provider } from 'react-redux'
+// Persist
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 // Saga
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './modules/sagas';
-// import thunk from 'redux-thunk';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
@@ -24,13 +26,16 @@ const store = createStore(
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
+        </PersistGate>
     </Provider>,
   document.getElementById('root')
 );
