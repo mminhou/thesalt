ca443487bde879ce7ec1533015f38546f0aeea00
R _ R <exit19093@gmail.com>
2021-07-17 14:21:13 +0900

    Fix: getProduct action, reducer, saga 함수 재정의

--- platform/src/modules/actions/productAction.js
+++ platform/src/modules/actions/productAction.js
@@ -1,23 +1,19 @@
-export const LOAD_PRODUCT = "LOAD_PRODUCT";
-export const LOAD_PRODUCT_SUCCESS = "LOAD_PRODUCT_SUCCESS";
-export const LOAD_PRODUCT_FAIL = "LOAD_PRODUCT_FAL";
+// All product
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+export const GET_PRODUCTS_SUCCESS = 'GET_PRODUCTS_SUCCESS';
+export const GET_PRODUCTS_ERROR = 'GET_PRODUCTS_ERROR';
+// Each product
+export const GET_PRODUCT = 'GET_PRODUCT';
+export const GET_PRODUCT_SUCCESS = 'GET_PRODUCT_SUCCESS';
+export const GET_PRODUCT_ERROR = 'GET_PRODUCT_ERROR';
 
-export const loadProduct = () => {
-    return{
-        type: LOAD_PRODUCT
-    };
-};
+export const getProducts = () => {return{ type: GET_PRODUCTS }};
 
-export const loadProductSuccess = products => {
-    return{
-        type: LOAD_PRODUCT_SUCCESS,
-        products: products
-    };
-};
-
-export const loadProductFail = error => {
-    return{
-        type: LOAD_PRODUCT_FAIL,
-        error
-    };
+// payload는 파라미터 용도, meta는 리듀서에서 id를 알기위한 용도
+export const getProduct = id => {
+    return {
+        type: GET_PRODUCT,
+        payload: id,
+        meta: id
+    }
 };
--- platform/src/modules/reducers/productReducer.js
+++ platform/src/modules/reducers/productReducer.js
@@ -1,19 +1,40 @@
-import { LOAD_PRODUCT, LOAD_PRODUCT_FAIL, LOAD_PRODUCT_SUCCESS } from '../actions/productAction'
+import { reducerUtils, handleAsyncActions, handleAsyncActionsById} from '../../lib/asyncUtils';
+import allAction from '../actions/index';
 
-const products = (state = [], action) => {
-    switch(action.type){
-        case LOAD_PRODUCT_SUCCESS:
-            return [...state, ...action.products];
-        case LOAD_PRODUCT_FAIL:
-            return [...state, action.error];
-        case "LOADED_PRODUCT":
-            return {
-                status: "complete",
-                products: action.products,
-            };
-        default:
-            return state;
-    }
+const initialProductState = {
+  products: reducerUtils.initial(),
+  product: reducerUtils.initial()
 };
+
+const products = (state = initialProductState, action) => {
+  switch (action.type) {
+    case allAction.GET_PRODUCTS:
+    case allAction.GET_PRODUCTS_SUCCESS:
+    case allAction.GET_PRODUCTS_ERROR:
+      return handleAsyncActions("GET_PRODUCTS", 'products', true)(state, action);
+    // case GET_POST:
+    // case GET_POST_SUCCESS:
+    // case GET_POST_ERROR:
+    //   return handleAsyncActionsById(GET_POST, 'post', true)(state, action);
+    default:
+      return state;
+  }
+}
+
+// const products = (state = [], action) => {
+//     switch(action.type){
+//         case LOAD_PRODUCT_SUCCESS:
+//             return [...state, ...action.products];
+//         case LOAD_PRODUCT_FAIL:
+//             return [...state, action.error];
+//         case "LOADED_PRODUCT":
+//             return {
+//                 status: "complete",
+//                 products: action.products,
+//             };
+//         default:
+//             return state;
+//     }
+// };
 
 export default products;
