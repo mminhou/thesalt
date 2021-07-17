b9f8bc0eaaf2e419f21c823ea04355e2334aa243
R _ R <exit19093@gmail.com>
2021-07-17 14:32:28 +0900

    Fix: products action 수정작업

--- platform/src/modules/actions/index.js
+++ platform/src/modules/actions/index.js
@@ -1,11 +1,17 @@
-import { loadProduct, loadProductFail, loadProductSuccess } from './productAction';
+import { getProducts, GET_PRODUCTS, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_ERROR, getProduct, GET_PRODUCT_SUCCESS, GET_PRODUCT_ERROR} from './productAction';
 import { signIn, signInFail, signInSuccess } from "./authAction";
 
 const allAction = {
+    // products
+    getProducts,
+    GET_PRODUCTS,
+    GET_PRODUCTS_SUCCESS,
+    GET_PRODUCTS_ERROR,
     // product
-    loadProduct,
-    loadProductSuccess,
-    loadProductFail,
+    getProduct,
+    GET_PRODUCT_SUCCESS,
+    GET_PRODUCT_ERROR,
+
     // signIn
     signIn,
     signInFail,
