2fe1121b78447b6cfcd598c4f044ba56d9bc2c3d
R _ R <exit19093@gmail.com>
2021-07-17 14:25:24 +0900

    Fix: getProduct data 호출 후 map 함수 오류 개선

--- platform/src/components/Products/Products.js
+++ platform/src/components/Products/Products.js
@@ -1,24 +1,19 @@
 import React, { useEffect } from 'react';
-import introduce from '../factory/images/introduce.jpg'
+import introduce from '../../factory/images/introduce.jpg'
 import {Grid} from "@material-ui/core";
-import MediaCard from "../components/MediaCard/MediaCard";
+import MediaCard from "../MediaCard/MediaCard";
 
-// ---------
 import { useDispatch, useSelector } from 'react-redux';
-import allAction from '../modules/actions/index';
+import allAction from '../../modules/actions';
 
-const About = () => {
-    const result = useSelector(state => state.products);
+const Products = () => {
+    const { data, loading, error } = useSelector(state => state.products.products);
     const dispatch = useDispatch();
-    console.log("success");
-    console.log(result);
-
 
     useEffect(() => {
-        // if (result) return;
-        // console.log(result)
-        dispatch(allAction.loadProduct());
-    }, [dispatch]);
+        if (data) return;
+        dispatch(allAction.getProducts());
+    }, [data, dispatch]);
 
     return (
         <div style={{height: 2000, backgroundColor: 'rgb(235, 234, 229)'}}>
@@ -27,33 +22,15 @@ const About = () => {
                   direction="row"
                   justify="center"
                   alignItems="center" style={{marginTop: 30}}>
-                {result.map(item => (
-                    <Grid item xs={3} style={{marginRight: 10, marginBottom: 20}}>
+                {data && data.map((item) =>
+                    <Grid key={item.style_code} xs={3} style={{marginRight: 10, marginBottom: 20}}>
                         <MediaCard item={item}></MediaCard>
                     </Grid>
-                ))}
-
-                {/*<Grid item xs={3} style={{marginRight: 10, marginBottom: 20}}>*/}
-                {/*    <MediaCard></MediaCard>*/}
-                {/*</Grid>*/}
-                {/*<Grid item xs={3} style={{marginRight: 10, marginBottom: 20}}>*/}
-                {/*    <MediaCard></MediaCard>*/}
-                {/*</Grid>*/}
-                {/*<Grid item xs={3} style={{marginRight: 10, marginBottom: 20}}>*/}
-                {/*    <MediaCard></MediaCard>*/}
-                {/*</Grid>*/}
-                {/*<Grid item xs={3} style={{marginRight: 10, marginBottom: 20}}>*/}
-                {/*    <MediaCard></MediaCard>*/}
-                {/*</Grid>*/}
-                {/*<Grid item xs={3} style={{marginRight: 10, marginBottom: 20}}>*/}
-                {/*    <MediaCard></MediaCard>*/}
-                {/*</Grid>*/}
-
-
+                )}
 
             </Grid>
         </div>
     );
 };
 
-export default About;
+export default Products;
