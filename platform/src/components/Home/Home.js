68e97e852df56f7c7f15fd76ce7b36cdbb43f539
R _ R <exit19093@gmail.com>
2021-08-30 00:31:00 +0900

    Feat: changed home background

--- platform/src/components/Home/Home.js
+++ platform/src/components/Home/Home.js
@@ -3,6 +3,7 @@ import {Grid} from "@material-ui/core";
 import {HomePagination} from "../Pagination/HomePagination";
 import Footer from "../Footer/Footer";
 import HomeCarousel from "../Carousel/HomeCarousel";
+import introduce from "../../factory/images/introduce2.jpeg";
 
 const Home = () => {
 
@@ -10,12 +11,13 @@ const Home = () => {
         <div style={{backgroundColor: 'rgb(235, 234, 229)'}}>
             <Grid container style={{textAlign: "center"}}>
                 <Grid item xs={12}>
-                    <div style={{borderBottom: '1px solid black', height: 500, marginBottom: 20}}>
-                        <HomeCarousel />
+                    <div style={{position: 'relative', height: '30%', marginBottom: 20}}>
+                        {/*<HomeCarousel />*/}
+                        <img src={introduce} style={{filter: 'brightness(60%)', width:'100%'}}/>
+
                     </div>
                 </Grid>
                 <Grid item xs={12} style={{  height: '100%' }}>
-                    New pagination 6 * 3
                     <HomePagination />
                 </Grid>
                 <Grid item xs={12} style={{ height: 150 }}>
