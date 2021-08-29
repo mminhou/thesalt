a6ca64c27fd21f4a8a89de2041405174fb677135
R _ R <exit19093@gmail.com>
2021-08-30 00:32:42 +0900

    Feat: using useEffect() for isLoggedIn state updating

--- platform/src/components/Login/SignIn.js
+++ platform/src/components/Login/SignIn.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './SignIn.sass';
 import {useDispatch, useSelector} from "react-redux";
 import {signIn, signOut} from "../../modules/actions/authAction";
 import {useHistory} from "react-router-dom";
+import {persistConfig} from "../../modules/reducers";
 
 const SignIn = () => {
-    const dispatch = useDispatch();
     const history = useHistory();
+    const dispatch = useDispatch();
     const { isLoggedIn } = useSelector(state => state.signIn)
-    console.log(isLoggedIn)
 
     const signInEvent = (e) => {
         e.preventDefault();
@@ -17,20 +17,18 @@ const SignIn = () => {
             password: e.target.password.value
         };
         dispatch(signIn(signInData));
-        history.push('/');
-
-        // service.fetchUserByEmailANDPassword(signInData)
-        /* TODO
-        1) 세션 또는 쿠키 관리
-        2) 페이지 이동(컴포넌트 unmount인지 ??)
-        */
     }
-
+    useEffect(() => {
+        if (isLoggedIn) {
+            history.push('/');
+            console.log("성공")
+        }
+    })
     // signOut test
-    // const signOutEvent = (e) => {
-    //     dispatch(signOut());
-    //     history.push('/login');
-    // }
+    const signOutEvent = (e) => {
+        dispatch(signOut());
+        history.push('/login');
+    }
 
     return (
         <form className='account-from' onSubmit={signInEvent}>
