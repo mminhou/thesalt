8942530287201df1c3792f1485acf0c8a863c4e4
R _ R <exit19093@gmail.com>
2021-08-31 00:33:40 +0900

    Feat: updated index

--- platform/src/modules/actions/index.js
+++ platform/src/modules/actions/index.js
@@ -1,5 +1,7 @@
-import { getProducts, GET_PRODUCTS, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_ERROR, getProduct, GET_PRODUCT_SUCCESS, GET_PRODUCT_ERROR} from './productAction';
+import { getProducts, GET_PRODUCTS, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_ERROR, getProduct, GET_PRODUCT, GET_PRODUCT_SUCCESS, GET_PRODUCT_ERROR} from './productAction';
 import { signIn, signInFail, signInSuccess } from "./authAction";
+import { GET_ACCOUNT, GET_ACCOUNT_SUCCESS, GET_ACCOUNT_ERROR, getAccount } from "./accountAction"
+import { ADD_PRODUCT_TO_CART, REMOVE_PRODUCT_FROM_CART, CATEGORIES_IS_LOADING, CATEGORIES_FETCH_DATA_SUCCESS } from "./shoppingCartAction";
 
 const allAction = {
     // products
@@ -9,9 +11,19 @@ const allAction = {
     GET_PRODUCTS_ERROR,
     // product
     getProduct,
+    GET_PRODUCT,
     GET_PRODUCT_SUCCESS,
     GET_PRODUCT_ERROR,
-
+    // account
+    getAccount,
+    GET_ACCOUNT,
+    GET_ACCOUNT_SUCCESS,
+    GET_ACCOUNT_ERROR,
+    // cart
+    ADD_PRODUCT_TO_CART,
+    REMOVE_PRODUCT_FROM_CART,
+    CATEGORIES_IS_LOADING,
+    CATEGORIES_FETCH_DATA_SUCCESS,
     // signIn
     signIn,
     signInFail,
--- platform/src/modules/reducers/index.js
+++ platform/src/modules/reducers/index.js
@@ -2,20 +2,28 @@ import {combineReducers} from 'redux';
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import {reducer as formReducer} from 'redux-form';
-import products from "./productReducer";
+import products  from "./productReducer";
+import account from "./accountReducer";
 import signIn from "./authReducer";
+import shoppingCart from "./shoppingCartReducer";
+import {categoriesIsLoading, categories} from "./shoppingCartReducer";
+
 
 export const persistConfig = {
   key: "root",
   // localStorage에 저장합니다.
   storage: storage,
-  whitelist: ["signIn"]
+  whitelist: ['signIn', 'shoppingCart', 'categories']
   // blacklist -> 그것만 제외합니다
 };
 
 const rootReducer = combineReducers({
     products,
     signIn,
+    account,
+    shoppingCart,
+    categoriesIsLoading,
+    categories,
     form: formReducer
 });
 
