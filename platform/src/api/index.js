620f06e5acc13ee8521881b5a0b7076682059882
R _ R <exit19093@gmail.com>
2021-09-01 20:15:59 +0900

    Feat: api url update

--- platform/src/api/index.js
+++ platform/src/api/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 // import { BASE_URL } from '../../config/config.json';
-
-const BASE_URL = 'http://localhost:8000/api';
-const AUTH_URL = 'http://localhost:8000/api-token-auth';
+// const BASE_URL = 'http://localhost:8000/api';
+// const AUTH_URL = 'http://localhost:8000/api-token-auth';
+const BASE_URL = 'http://ec2-3-36-116-59.ap-northeast-2.compute.amazonaws.com/api';
+const AUTH_URL = 'http://ec2-3-36-116-59.ap-northeast-2.compute.amazonaws.com/api-token-auth';
 
 const getProducts = () => {
     return axios.get(`${BASE_URL}/products/`);
 };
 const getProduct = (id) => {
-    // console.log(`${BASE_URL}/products/${id}/`);
     return axios.get(`${BASE_URL}/products/${id}/`);
 }
 
