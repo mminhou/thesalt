5ef845b891b55bb603e13f02a31fd5673c7d679a
R _ R <exit19093@gmail.com>
2021-09-01 16:45:40 +0900

    Feat: delete no use tag

--- platform/src/components/ShoppingCart/ShoppingCart.js
+++ platform/src/components/ShoppingCart/ShoppingCart.js
@@ -1,20 +1,18 @@
-import React, {useEffect} from 'react';
-import {
-    Button,
-    DialogTitle,
-    DialogContent,
-    DialogContentText,
-    DialogActions,
-    Grid,
-    Divider,
-    Typography,
-} from '@material-ui/core';
+import React from 'react';
+import {Button, DialogContent, DialogContentText, Grid, Divider, Typography,} from '@material-ui/core';
 import {connect} from "react-redux";
 import {removeProductFromCart} from "../../modules/actions/shoppingCartAction";
 
 const ShoppingCart = (props) => {
     const calculateTotal = (total, currentItem) =>
         parseFloat(total + currentItem.price * (currentItem.quantity || 1));
+
+    const countItems = () =>
+        props.shoppingCart
+            .reduce((acc, cur) => {
+                return parseFloat(acc + (cur.quantity || 1));
+            }, 0)
+            .toFixed(0);
 
     const renderProduct = (product, index) => (
         <Grid container key={index} direction="row" alignItems="center">
@@ -45,18 +43,9 @@ const ShoppingCart = (props) => {
         </Grid>
     );
 
-    const countItems = () =>
-        props.shoppingCart
-            .reduce((acc, cur) => {
-                return parseFloat(acc + (cur.quantity || 1));
-            }, 0)
-            .toFixed(0);
-
-
     return (
         <div>
             <Grid container>
-                {/*<DialogTitle id="alert-dialog-title" style={{width: 1000, fontSize: 50, fontVariant: 'small-caps'}}>{"cart"}</DialogTitle>*/}
                 <DialogContent style={{width: '100%'}}>
                     <DialogContentText id="alert-dialog-description">
                         <Grid item xs={12}>
@@ -97,18 +86,11 @@ const ShoppingCart = (props) => {
                         </Grid>
                         <Grid item xs={12}>
                             <Divider style={{marginTop: 10, marginBottom: 20}}/>
-                            <Button variant="contained" color="secondary" style={{width: '100%'}} >complete your order</Button>
+                            <Button variant="contained" color="secondary" style={{width: '100%'}}>complete your
+                                order</Button>
                         </Grid>
                     </DialogContentText>
                 </DialogContent>
-                <DialogActions>
-                    {/*<Button onClick={this.closeModal} color="primary">*/}
-                    {/*    Disagree*/}
-                    {/*</Button>*/}
-                    {/*<Button onClick={this.closeModal} color="primary" autoFocus>*/}
-                    {/*    Agree*/}
-                    {/*</Button>*/}
-                </DialogActions>
             </Grid>
         </div>
     )
