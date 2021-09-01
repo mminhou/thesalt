7430ef5ef5355d6594a34c1243035a480d2fa17b
R _ R <exit19093@gmail.com>
2021-09-01 18:49:27 +0900

    Feat: conetents update:

--- platform/src/components/Pagination/HomePagination.js
+++ platform/src/components/Pagination/HomePagination.js
@@ -1,17 +1,8 @@
 import React, {useEffect} from 'react';
-import {Link, useHistory} from 'react-router-dom';
-
 import './HomePagination.css';
-import {Grid, Typography} from "@material-ui/core";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import image from '../../factory/images/default-404.jpg'
-import {Fade} from '@material-ui/core';
-
-import Button from '@material-ui/core/Button';
 import {useDispatch, useSelector} from "react-redux";
+import {Link} from 'react-router-dom';
+import {Grid, Typography, Card, CardActionArea, CardContent, CardMedia} from "@material-ui/core";
 import allAction from "../../modules/actions";
 
 export function HomePagination() {
@@ -31,7 +22,6 @@ export function HomePagination() {
     }, [data, contents, dispatch]);
 
     let listsPerPage = 6;
-
     let maxPages = Math.ceil(contents.length / listsPerPage);
     let items = [];
     let leftSide = currentPage - 2;
@@ -49,40 +39,26 @@ export function HomePagination() {
             </div>,
         );
     }
-
     const nextPage = () => {
         if (currentPage < maxPages) {
             setCurrentPage(currentPage + 1)
         }
     }
-
     const prevPage = () => {
         if (currentPage > 1) {
             setCurrentPage(currentPage - 1)
         }
     }
 
-    // Logic for displaying current todos
     const indexOfLastList = currentPage * listsPerPage;
     const indexOfFirstList = indexOfLastList - listsPerPage;
     const currentLists = contents.slice(indexOfFirstList, indexOfLastList);
-
-
     const paginationRender = (
         <div style={{paddingLeft: '10%', paddingRight: '10%'}}>
             <Grid container direction="column" alignItems="center">
-                <Grid container direction="row" alignItems="center"
-                      style={{marginBottom: 20, paddingBottom: 5, borderBottom: '2px solid #787878'}}>
+                <Grid container className="title-container"  direction="row" alignItems="center">
                     <Grid item xs={6} justify="flex-start">
-                        <Typography variant="h4" style={{
-                            float: 'left',
-                            fontSize: 50,
-                            fontWeight: 900,
-                            fontVariant: "small-caps",
-                            color: '#404040'
-                        }}>
-                            new arrivals
-                        </Typography>
+                        <Typography variant="h3" className="pagination-title">new arrivals</Typography>
                     </Grid>
                     <Grid item xs={6}>
                         <div className="paginate-ctn" style={{float: 'right'}}>
@@ -94,16 +70,14 @@ export function HomePagination() {
                 </Grid>
 
                 <Grid container direction="row" justify="center" alignItems="center" justifyContent="center">
-                    {/*<div className="">*/}
-                    {/*    <div> currentPage : { currentPage }</div>*/}
                     {currentLists.map((ele) => ele.subCategory === 'new' ?
                         <Grid item lg={2} md={3} sm={4}>
-                            <Card style={{marginRight: 10, border: "none", boxShadow: "none"}}>
-                                <CardActionArea style={{backgroundColor: "rgb(235, 234, 229)", border: "none"}}>
+                            <Card className="pagination-card">
+                                <CardActionArea className="pagination-card-action">
                                     <Link to={'/productDetail/'+ele.styleCode}>
                                         <CardMedia image={ele.mainImage} style={{height: 150}}
                                                    title="Contemplative Reptile"/>
-                                        <CardContent style={{paddingLeft: 0, paddingRight: 0, paddingBottom: 10, paddingTop: 10}}>
+                                        <CardContent className="pagination-card-content">
                                             <Typography gutterBottom variant="body2" noWrap='True'>
                                                 {ele.subCategory}
                                             </Typography>
