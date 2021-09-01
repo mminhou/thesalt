dda07e1a82ba951492c890950262809a4bb8d4a9
R _ R <exit19093@gmail.com>
2021-09-01 18:05:26 +0900

    Feat: line cleaning

--- platform/src/components/ProductCard/ProductCard.js
+++ platform/src/components/ProductCard/ProductCard.js
@@ -1,27 +1,9 @@
 import React, {useCallback} from 'react';
+import './ProductCard.css'
 import {useHistory} from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import LinearProgress from "@material-ui/core/LinearProgress";
-
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 300,
-  },
-});
+import {Card, CardActionArea, CardMedia} from '@material-ui/core';
 
 export default function ProductCard({item}) {
-  const classes = useStyles();
   const history = useHistory();
   const handleOnClick = useCallback(() => history.push(
       '/productDetail/' + item.styleCode), [history]);
@@ -30,31 +12,11 @@ export default function ProductCard({item}) {
     <Card>
       <CardActionArea onClick={handleOnClick}>
         <CardMedia
-          className={classes.media}
+          className="media"
           image={item.mainImage}
           title={item.title}
         />
       </CardActionArea>
-      {/*  <CardContent>*/}
-      {/*    <Typography gutterBottom variant="h5" component="h2">*/}
-      {/*      {item.style_code}*/}
-      {/*    </Typography>*/}
-      {/*    <Typography variant="body2" color="textSecondary" component="p">*/}
-      {/*      {item.product_detail}*/}
-      {/*    </Typography>*/}
-      {/*  </CardContent>*/}
-      {/*  <CardContent>*/}
-      {/*    <LinearProgress variant="determinate" value={30} />*/}
-      {/*  </CardContent>*/}
-      {/*</CardActionArea>*/}
-      {/*<CardActions>*/}
-      {/*  <Button size="small" color="primary">*/}
-      {/*    30%*/}
-      {/*  </Button>*/}
-      {/*  <Button size="small" color="primary">*/}
-      {/*    1,000,000₩*/}
-      {/*  </Button>*/}
-      {/*</CardActions>*/}
     </Card>
   );
 }
