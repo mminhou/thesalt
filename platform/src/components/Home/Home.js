523ebc45429cb47b4619c6e46911f2af8b253f19
R _ R <exit19093@gmail.com>
2021-09-01 20:13:47 +0900

    Feat: add pagination function

--- platform/src/components/Home/Home.js
+++ platform/src/components/Home/Home.js
@@ -1,34 +1,28 @@
-import React, {useEffect} from 'react';
-import {Grid} from "@material-ui/core";
+import React from 'react';
+import './Home.css';
 import {HomePagination} from "../Pagination/HomePagination";
 import Footer from "../Footer/Footer";
+import {Grid} from "@material-ui/core";
 import introduce from "../../factory/images/introduce2.jpeg";
-// import {useDispatch, useSelector} from 'react-redux';
-import allAction from "../../modules/actions";
-import {useSelector} from "react-redux";
 
 const Home = () => {
-
     return (
-        <div style={{backgroundColor: 'rgb(235, 234, 229)'}}>
+        <div className="home-container">
             <Grid container style={{textAlign: "center"}}>
                 <Grid item xs={12} md={12}>
-                    <div style={{position: 'relative', height: '30%', marginBottom: 30}}>
-                        <img src={introduce} style={{filter: 'brightness(60%)', width:'100%'}}/>
+                    <div className="banner-image">
+                        <img src={introduce}/>
                     </div>
                 </Grid>
-                <Grid item xs={12} style={{  height: '100%' }}>
-                    <HomePagination />
+                <Grid item xs={12}>
+                    <HomePagination/>
                 </Grid>
-                <Grid item xs={12} style={{ height: 150 }}>
-                    {/*open project -> link page(create board)*/}
+                <Grid item xs={12} style={{height: 150}}>
                 </Grid>
                 <Grid item xs={12}>
-                    <Footer />
+                    <Footer/>
                 </Grid>
-                {/* mainImage, title, funding, name */}
             </Grid>
-
         </div>
     );
 };
