f4876c0da3de4acaf8255cf87dc9616b4f8705c6
R _ R <exit19093@gmail.com>
2021-09-01 16:42:34 +0900

    Feat: update asyncUtil

--- platform/src/lib/asyncUtils.js
+++ platform/src/lib/asyncUtils.js
@@ -1,11 +1,9 @@
 import { call, put } from 'redux-saga/effects';
 
-// 프로미스를 기다렸다가 결과를 디스패치하는 사가
 export const createPromiseSaga = (type, promiseCreator) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
   return function* saga(action) {
     try {
-      // 재사용성을 위하여 promiseCreator 의 파라미터엔 action.payload 값을 넣도록 설정합니다.
       const payload = yield call(promiseCreator, action.payload);
       yield put({ type: SUCCESS, payload });
     } catch (e) {
@@ -14,9 +12,6 @@ export const createPromiseSaga = (type, promiseCreator) => {
   };
 };
 
-// 특정 id의 데이터를 조회하는 용도로 사용하는 사가
-// API를 호출 할 때 파라미터는 action.payload를 넣고,
-// id 값을 action.meta로 설정합니다.
 export const createPromiseSagaById = (type, promiseCreator) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
   return function* saga(action) {
@@ -30,17 +25,12 @@ export const createPromiseSagaById = (type, promiseCreator) => {
   };
 };
 
-// 리듀서에서 사용 할 수 있는 여러 유틸 함수들입니다.
 export const reducerUtils = {
-  // 초기 상태. 초기 data 값은 기본적으로 null 이지만
-  // 바꿀 수도 있습니다.
   initial: (initialData = null) => ({
     loading: false,
     data: initialData,
     error: null
   }),
-  // 로딩중 상태. prevState의 경우엔 기본값은 null 이지만
-  // 따로 값을 지정하면 null 로 바꾸지 않고 다른 값을 유지시킬 수 있습니다.
   loading: (prevState = null) => ({
     loading: true,
     data: prevState,
@@ -60,8 +50,6 @@ export const reducerUtils = {
   })
 };
 
-// 비동기 관련 액션들을 처리하는 리듀서를 만들어줍니다.
-// type 은 액션의 타입, key 는 상태의 key (예: posts, post) 입니다.
 export const handleAsyncActions = (type, key, keepData = false) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
   return (state, action) => {
@@ -87,7 +75,6 @@ export const handleAsyncActions = (type, key, keepData = false) => {
   };
 };
 
-// id별로 처리하는 유틸함수
 export const handleAsyncActionsById = (type, key, keepData = false) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
   return (state, action) => {
@@ -99,7 +86,6 @@ export const handleAsyncActionsById = (type, key, keepData = false) => {
           [key]: {
             ...state[key],
             [id]: reducerUtils.loading(
-              // state[key][id]가 만들어져있지 않을 수도 있으니까 유효성을 먼저 검사 후 data 조회
               keepData ? state[key][id] && state[key][id].data : null
             )
           }
