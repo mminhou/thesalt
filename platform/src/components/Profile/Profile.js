9038705f3013129e4e81857253db2f5820cda3a6
R _ R <exit19093@gmail.com>
2021-09-01 17:08:14 +0900

    Feat: Line cleaning

--- platform/src/components/Profile/Profile.js
+++ platform/src/components/Profile/Profile.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import {Grid, Typography} from "@material-ui/core";
 import {persistConfig} from "../../modules/reducers";
-import allAction from "../../modules/actions";
+import {Grid, Typography} from "@material-ui/core";
 import avatar from "../../factory/images/avatar.jpg";
 
 const Profile = () => {
@@ -20,20 +19,15 @@ const Profile = () => {
     return (
         <div>
             <div style={{backgroundColor: '#483D8B', height: 120}}></div>
-            {account ? (
+            { account ? (
                 <Grid container direction="row" justify="center" alignItems="center" style={{textAlign: "center"}}>
                     <Grid item xs={12} style={{marginTop: 35}}>
-                        <img src={avatar} width='15%' style={{borderRadius: '50%'}}/>
+                        <img src={avatar} width="15%" className="avatar"/>
                         <Typography variant="h6">Welcome!</Typography>
                         <Typography variant="h6">Hope you will have a great time working with us</Typography>
                         <br/>
                         <Typography variant="body1">{account.email}</Typography>
                     </Grid>
-
-                    {/*<Grid item xs={12} style={{marginTop: 50}}>*/}
-                    {/*    <img src={avatar} width='25%' style={{borderRadius: '50%'}}/>*/}
-
-                    {/*</Grid>*/}
                 </Grid>
             ) : (
                 <div>does not exist account</div>
