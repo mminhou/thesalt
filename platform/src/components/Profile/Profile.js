d92133941fd42835f60a9d2ee9edcffefcdcfb55
R _ R <exit19093@gmail.com>
2021-09-05 22:07:44 +0900

    Feat: Profile 컴포넌트 업데이트, 정보 변경 페이지 추가

--- platform/src/components/Profile/Profile.js
+++ platform/src/components/Profile/Profile.js
@@ -1,36 +1,45 @@
 import React, {useState, useEffect} from 'react';
 import {persistConfig} from "../../modules/reducers";
-import {Grid, Typography} from "@material-ui/core";
+import {Button, Grid, Typography} from "@material-ui/core";
 import avatar from "../../factory/images/avatar.jpg";
+import {BuildTwoTone} from "@material-ui/icons";
+import {Link} from "react-router-dom";
 
 const Profile = () => {
     const [account, setAccount] = useState("")
+
     useEffect(() => {
         persistConfig.storage.getItem("account").then(res => {
             if (res && !account) {
                 setAccount(JSON.parse(res))
-            };
-        });
-        persistConfig.storage.getItem("token").then(res2 => {
-            // console.log(JSON.parse(res2))
+            }
+            ;
         });
     })
 
     return (
         <div>
             <div style={{backgroundColor: '#483D8B', height: 120}}></div>
-            { account ? (
+            {account ? (
                 <Grid container direction="row" justify="center" alignItems="center" style={{textAlign: "center"}}>
                     <Grid item xs={12} style={{marginTop: 35}}>
                         <img src={avatar} width="15%" className="avatar"/>
-                        <Typography variant="h6">Welcome!</Typography>
-                        <Typography variant="h6">Hope you will have a great time working with us</Typography>
+                        <Typography variant="h6">Welcome! {account.fitst_name}</Typography>
+                        <Typography variant="h6">Hope you will have a great shopping time with us</Typography>
                         <br/>
                         <Typography variant="body1">{account.email}</Typography>
                     </Grid>
+                    <Grid item xs={6}>
+                        <Link to='/myAccount'><Button>내정보 수정하기</Button></Link>
+                    </Grid>
+                    <Grid item xs={6}>
+                        주문 조회
+                    </Grid>
                 </Grid>
             ) : (
-                <div>does not exist account</div>
+                <Grid container direction="row" justify="center" alignItems="center" style={{textAlign: "center"}}>
+                    <div>does not exist account</div>
+                </Grid>
             )}
         </div>
     )
