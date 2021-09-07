957f64778145336624145bae3e6969f5d924c0d0
R _ R <exit19093@gmail.com>
2021-09-08 00:50:02 +0900

    Refactor: shopping cart connect -> changed useSelector, dispatch

--- platform/src/components/ProductCard/ProductCardDetail.js
+++ platform/src/components/ProductCard/ProductCardDetail.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './ProductCardDetail.css';
 import {useDispatch, useSelector, connect} from "react-redux";
 import { addProductToCart } from "../../modules/actions/shoppingCartAction";
@@ -9,22 +9,25 @@ import Footer from "../Footer/Footer";
 const ProductCardDetail = (props) => {
     const params = props.match.params.path;
     const dispatch = useDispatch();
-    const {data, loading, error} = useSelector(
-        state => state.products.product[params]) || {
+    const {data, loading, error} = useSelector(state => state.products.product[params]) || {
         loading: false, data: null, error: null};
-    const [secondary, setSecondary] = React.useState(false);
+    const [secondary, setSecondary] = useState(false);
 
     useEffect(() => {
         dispatch(allAction.getProduct(params));
     }, [dispatch, params]);
+
+    const addProduct = (product) => {
+        dispatch(addProductToCart(product))
+    }
 
     return (
         <div>
             <div style={{backgroundColor: 'rgb(25,25,25)', height: 120}}></div>
             <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30, textAlign: "center"}}>
                 {data ? (
-                    <Grid container xs={12} direction="row" alignItems="center">
-                        <Grid container xs={8} direction="row" style={{paddingLeft: '5%', paddingRight: '3%'}}>
+                    <Grid container item xs={12} direction="row" alignItems="center">
+                        <Grid container item xs={8} direction="row" style={{paddingLeft: '5%', paddingRight: '3%'}}>
                             <Grid item xs={6} className="padding-right">
                                 <img src={data.mainImage} width="100%" alt={data.title}/>
                             </Grid>
@@ -45,7 +48,7 @@ const ProductCardDetail = (props) => {
                             <Typography variant="h5">{data.title}</Typography>
                             <Typography variant="subtitle1">{data.subtitle}</Typography>
                             <br/><br/>
-                            <Typography variant="h6">₩ {data.price.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}<span className="price-tax"> (included tax) </span></Typography>
+                            <Typography variant="h6">$ {data.price.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}<span className="price-tax"> (included tax) </span></Typography>
                             <br/><br/>
                             <List className="product-info">
                                 <ListItem style={{textAlign: 'center'}}>
@@ -61,7 +64,7 @@ const ProductCardDetail = (props) => {
                             <Typography variant="body1">{data.detail}</Typography>
                             <br/><br/>
                             <Button variant="outlined" color="secondary"
-                                    onClick={() => {props.addProduct(data); alert("Item Successfully added to your cart")}}
+                                    onClick={() => {addProduct(data); alert("Item Successfully added to your cart")}}
                                     className="cart-button">Add cart</Button>
                         </Grid>
                         <Grid item xs={12} style={{marginTop: '5%'}}>
@@ -74,20 +77,4 @@ const ProductCardDetail = (props) => {
         </div>
     );
 }
-
-const mapStateToProps = state => {
-  return {
-    shoppingCart: state.shoppingCart
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    addProduct: product => dispatch(addProductToCart(product))
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProductCardDetail);
+export default ProductCardDetail;
--- platform/src/components/ShoppingCart/ShoppingCart.js
+++ platform/src/components/ShoppingCart/ShoppingCart.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button, DialogContent, DialogContentText, Grid, Divider, Typography,} from '@material-ui/core';
-import {connect} from "react-redux";
+import {connect, useDispatch, useSelector} from "react-redux";
 import {removeProductFromCart} from "../../modules/actions/shoppingCartAction";
+import {Link} from "react-router-dom";
 
-const ShoppingCart = (props) => {
+const ShoppingCart = () => {
+    const shoppingCart = useSelector(state => state.shoppingCart);
+    const dispatch = useDispatch();
+
     const calculateTotal = (total, currentItem) =>
         parseFloat(total + currentItem.price * (currentItem.quantity ? currentItem.quantity : 1));
 
     const countItems = () =>
-        props.shoppingCart
+        shoppingCart
             .reduce((acc, cur) => {
                 return parseFloat(acc + (cur.quantity ? cur.quantity : 1));
             }, 0)
             .toFixed(0);
+
+    const removeProduct = (index) => {
+        dispatch(removeProductFromCart(index))
+    }
+
 
     const renderProduct = (product, index) => (
         <Grid container key={index} direction="row" alignItems="center">
@@ -35,80 +44,80 @@ const ShoppingCart = (props) => {
             <Grid item xs={1}>
                 <input
                     type="button"
-                    onClick={() => props.removeProduct(index)}
+                    onClick={() => removeProduct(index)}
                     value="x"
                 />
             </Grid>
-
         </Grid>
     );
 
     return (
-        <div>
-            <Grid container>
-                <DialogContent style={{width: '100%'}}>
-                    <DialogContentText id="alert-dialog-description">
-                        <Grid item xs={12}>
-                            <Typography variant="h4" style={{fontVariant: 'small-caps'}}>
-                                <span style={{fontSize: 25}}> {countItems()},</span> items in bag</Typography>
-                        </Grid>
-                        <Divider style={{marginTop: 10, marginBottom: 10}}/>
-                        <Grid item xs={12}>
-                            <div className="cart">
-                                {props.shoppingCart.length
-                                    ? props.shoppingCart.map(renderProduct)
-                                    : <Grid item style={{width: 400}}>
+        <Grid container>
+            <DialogContent style={{width: '100%'}}>
+                <DialogContentText id="alert-dialog-description">
+                    <Grid item xs={12}>
+                        <Typography variant="h4" style={{fontVariant: 'small-caps'}}>
+                            <span style={{fontSize: 25}}> {countItems()},</span> items in bag</Typography>
+                    </Grid>
+                    <Divider style={{marginTop: 10, marginBottom: 10}}/>
+                    <Grid item xs={12}>
+                        <div className="cart">
+                            {shoppingCart.length
+                                ? shoppingCart.map(renderProduct)
+                                : <Grid item style={{width: 400}}>
 
-                                    </Grid>}
-                            </div>
-                        </Grid>
-                        <Grid item xs={12}>
-                            {props.shoppingCart.length ? (
-                                <Grid container>
-                                    <Grid item xs={12}><Divider style={{marginTop: 10, marginBottom: 10}}/></Grid>
-                                    <Grid item xs={6}>
-                                        <Typography variant="body1" style={{
-                                            fontVariant: 'small-caps',
-                                            fontSize: 20
-                                        }}>subtotal</Typography>
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        <Typography variant="h6" style={{float: 'right'}}>
-                                            $ {props.shoppingCart.reduce(calculateTotal, 0).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                                        </Typography>
-                                    </Grid>
+                                </Grid>}
+                        </div>
+                    </Grid>
+                    <Grid item xs={12}>
+                        {shoppingCart.length ? (
+                            <Grid container>
+                                <Grid item xs={12}><Divider style={{marginTop: 10, marginBottom: 10}}/></Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant="body1" style={{
+                                        fontVariant: 'small-caps',
+                                        fontSize: 20
+                                    }}>subtotal</Typography>
                                 </Grid>
-                            ) : (
-                                <Typography variant="h6" style={{textAlign: "center", fontVariant: 'small-caps'}}>
-                                    your shopping bag is empty
-                                </Typography>
-                            )}
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Divider style={{marginTop: 10, marginBottom: 20}}/>
+                                <Grid item xs={6}>
+                                    <Typography variant="h6" style={{float: 'right'}}>
+                                        $ {shoppingCart.reduce(calculateTotal, 0).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        ) : (
+                            <Typography variant="h6" style={{textAlign: "center", fontVariant: 'small-caps'}}>
+                                your shopping bag is empty
+                            </Typography>
+                        )}
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider style={{marginTop: 10, marginBottom: 20}}/>
+                        <Link to='/order'>
                             <Button variant="contained" color="secondary" style={{width: '100%'}}>complete your
                                 order</Button>
-                        </Grid>
-                    </DialogContentText>
-                </DialogContent>
-            </Grid>
-        </div>
+                        </Link>
+                    </Grid>
+                </DialogContentText>
+            </DialogContent>
+        </Grid>
     )
 }
-
-const mapStateToProps = state => {
-    return {
-        shoppingCart: state.shoppingCart
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        removeProduct: index => dispatch(removeProductFromCart(index))
-    };
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ShoppingCart);
+export default ShoppingCart;
+//
+// const mapStateToProps = state => {
+//     return {
+//         shoppingCart: state.shoppingCart
+//     };
+// };
+//
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         removeProduct: index => dispatch(removeProductFromCart(index))
+//     };
+// };
+//
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(ShoppingCart);
