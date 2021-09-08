e6a6aa335628111ae5984707bd829bfec12bd8b6
R _ R <exit19093@gmail.com>
2021-09-08 13:18:14 +0900

    Feat: Screen component responsible UI 적용

--- platform/src/components/Screen/Screen.js
+++ platform/src/components/Screen/Screen.js
@@ -2,11 +2,18 @@ import React from "react";
 import './Screen.css'
 import Fullpage, {FullPageSections, FullpageSection, FullpageNavigation} from '@ap.cx/react-fullpage'
 import ShowMoreButton from "../ShowMoreButton/ShowMoreButton";
+import {createTheme, responsiveFontSizes, ThemeProvider} from '@material-ui/core/styles';
+
+
 import {Grid, Typography} from "@material-ui/core"
 import video from '../../factory/v1.mp4'
 import min from '../../factory/images/min.png'
 
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 export function Screen() {
+
     return (
         <Fullpage>
             <FullpageNavigation></FullpageNavigation>
@@ -16,14 +23,16 @@ export function Screen() {
                         <source src={video} type="video/mp4"/>
                     </video>
                     <Grid container style={{marginTop: 300, top: 0, left: 0, position: "absolute"}}>
-                        <Grid item xs={6}></Grid>
-                        <Grid item xs={6}>
+                        <Grid item md={6} xs={2}></Grid>
+                        <Grid item md={6} xs={10}>
                             <div style={{paddingRight: '15%'}}>
-                                <h1 className="title">THE SALT</h1>
-                                <h1 className="subtitle">High Quality Select Shop</h1>
-                                <h1 className="buttonText">
-                                    <ShowMoreButton label="show more"/>
-                                </h1>
+                                <ThemeProvider theme={theme}>
+                                    <Typography variant="h1" className="title">THE SALT</Typography>
+                                    <Typography variant="h3" className="subtitle">High Quality Select Shop</Typography>
+                                    <Typography variant="h3" className="buttonText">
+                                        <ShowMoreButton label="show more"/>
+                                    </Typography>
+                                </ThemeProvider>
                             </div>
                         </Grid>
                     </Grid>
@@ -32,13 +41,17 @@ export function Screen() {
                     <Grid container direction="row" justify="center" alignItems="center" style={{textAlign: "center"}}>
                         <Grid item xs={12}>
                             <div style={{height: 120}}></div>
-                            <Typography variant="h4" className="introduce">CREATOR</Typography>
-                            <img src={min} width='25%' className="avatar"/>
-                            <Typography variant="h4" calssName="smallCaps">minho choi</Typography>
-                            <Typography variant="h5" className="smallCaps">computer science</Typography>
-                            <Typography variant="h5" className="info"><a href='https://github.com/mminhou/cntex'>https://github.com/mminhou/cntex</a></Typography>
-                            <Typography variant="h5" className="info">exit19093@gmail.com</Typography>
-                            <Typography variant="h5" className="info">010-2895-1359</Typography>
+                            <ThemeProvider theme={theme}>
+                                <Typography variant="h3" className="introduce">CREATOR</Typography>
+                                <img src={min} width='280px' className="avatar"/>
+                                <Typography variant="h4" className="smallCaps">minho choi</Typography>
+                                <Typography variant="h5" className="smallCaps">computer science</Typography>
+                                <Typography variant="h5" className="info">
+                                    <a href='https://github.com/mminhou/cntex'>https://github.com/mminhou/cntex</a>
+                                </Typography>
+                                <Typography variant="h5" className="info">exit19093@gmail.com</Typography>
+                                <Typography variant="h5" className="info">010-2895-1359</Typography>
+                            </ThemeProvider>
                         </Grid>
                     </Grid>
                 </FullpageSection>
