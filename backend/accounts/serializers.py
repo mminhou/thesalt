3cddef181be9a3f6801aaf0492c7f218867612b9
R _ R <exit19093@gmail.com>
2021-09-08 16:49:20 +0900

    Refactor: create accout view 생성, hash password 업데이트

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 from .models import User
 from order.serializers import OrderSerializer
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     Order = OrderSerializer(read_only=True, many=True)
-
     class Meta:
         model = User
         fields = ('id', 'email', 'password',
                   'first_name', 'last_name', 'city', 'address1', 'address2',
                   'zip_code', 'contact_num', 'date_joined', 'Order')
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    # def create(self, validated_data):
+    #     # password = validated_data.pop('password')
+    #     password = make_password(validated_data.pop('password'))
+    #     print(password)
+    #     user = User(**validated_data)
+    #     user.set_password(password)
+    #     user.save()
+    #     return user
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,19 +1,16 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework import generics
-from rest_framework.filters import SearchFilter
 from django.contrib.auth.hashers import make_password
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from rest_framework.decorators import api_view, permission_classes
-
+from rest_framework.decorators import permission_classes
 
 @permission_classes([IsAuthenticated])
 class UserViewSet(viewsets.ModelViewSet):
     authentication_classes = [JSONWebTokenAuthentication]
-
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -25,9 +22,6 @@ class UserViewSet(viewsets.ModelViewSet):
         return user
 
     def update(self, request, *args, **kwargs):
-        # user = User.objects.get(pk=self.request.META['HTTP_AUTHORIZATION'])
-        # user = User.objects.get(pk=self.request.data['id'])
-        # serializer.save(user=user)
         user = self.request.data
         User.objects.filter(id=user['id']).update(
             first_name=user['first_name'],
@@ -47,3 +41,14 @@ class UserViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(email=email)
         return queryset
 
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
+
+    # Encrypted password
+    def perform_create(self, serializer):
+        hashed_password = make_password(serializer.validated_data['password'])  # get the hashed password
+        serializer.validated_data['password'] = hashed_password
+        user = super(UserCreateView, self).perform_create(serializer)  # create user
+        return user
