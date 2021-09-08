4bcebebfeb352c87c190a8d9b1b08649d6f5baf0
R _ R <exit19093@gmail.com>
2021-09-08 22:59:37 +0900

    Feat: Products component responsible UI 적용

--- platform/src/components/Products/Products.js
+++ platform/src/components/Products/Products.js
@@ -1,9 +1,11 @@
 import React, {useEffect} from 'react';
+import './Products.css'
 import {useDispatch, useSelector} from 'react-redux';
 import allAction from '../../modules/actions';
-import {Grid, Tabs, Tab} from "@material-ui/core";
+import {Grid, Tabs, Tab, Typography} from "@material-ui/core";
 import ProductCard from "../ProductCard/ProductCard";
 import Footer from "../Footer/Footer";
+import withWidth, { isWidthUp } from "@material-ui/core/withWidth";
 
 function TabPanel(props) {
     const {children, value, index, ...other} = props;
@@ -23,13 +25,13 @@ function TabPanel(props) {
 };
 
 function a11yProps(index) {
-    return {
-        id: `vertical-tab-${index}`,
-        "aria-controls": `vertical-tabpanel-${index}`
-    };
-};
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
+}
 
-const Products = () => {
+const Products = (props) => {
     const {data, loading, error} = useSelector(state => state.products.products);
     const [value, setValue] = React.useState(0);
     const dispatch = useDispatch();
@@ -43,14 +45,21 @@ const Products = () => {
         setValue(newValue);
     };
 
+    let condTabOrientation;
+      if (isWidthUp("md", props.width)) {
+        condTabOrientation = "vertical";
+      } else {
+        condTabOrientation = "horizontal";
+      }
+
     const productsTemplate = (category) => {
         return (data && data.map(item =>
             item.mainCategory === category ? (
-                    <Grid xs={3} className="products-item" style={{marginRight: 20}}>
+                    <Grid key={item.id} item xs={8} md={3} className="products-item" spacing={5}>
                         <ProductCard item={item}></ProductCard>
                     </Grid>) :
                 (category === 'ALL' ? (
-                    <Grid key={item.styleCode} xs={3} className="products-item" style={{marginRight: 20}}>
+                    <Grid key={item.id} item xs={8} md={3} className="products-item" spacing={5}>
                         <ProductCard item={item}></ProductCard>
                     </Grid>
                 ) : (<div></div>))
@@ -58,15 +67,16 @@ const Products = () => {
     }
 
     return (
-        <Grid container justifyContent="center" style={{textAlign: 'center'}}>
+        <Grid container justify="center" style={{textAlign: 'center'}}>
             <Grid item xs={12}>
-                <div style={{backgroundColor: 'rgb(25,25,25)', height: 120}}></div>
-                <h1 className="products-title">Sales / Collection</h1>
-                <p className="products-subtitle">New in: hand-picked daily from the world’s best goods and boutiques</p>
+                <div style={{backgroundColor: 'rgb(25, 25, 25)', height: window.innerWidth <= 850 ? 55 : 120}}></div>
+                <Typography variant="h3" paragraph className="products-title" >Sales / Collection</Typography>
+                <Typography variant="subtitle1" paragraph className="products-subtitle">New in: hand-picked daily from the world’s best goods and boutiques</Typography>
             </Grid>
-            <Grid item xs={2}>
-                <p>CATEGORY</p>
-                <Tabs orientation="vertical" variant="scrollable" value={value} onChange={handleChange}>
+            <Grid item md={2} xs={12}>
+                <Typography variant="subtitle1">CATEGORY</Typography>
+                <Tabs orientation={condTabOrientation} variant="scrollable"
+                      value={value} onChange={handleChange} style={{width: '100%'}}>
                     <Tab label="ALL" {...a11yProps(0)} />
                     <Tab label="OUTER" {...a11yProps(1)} />
                     <Tab label="TOP" {...a11yProps(2)} />
@@ -75,34 +85,34 @@ const Products = () => {
                     <Tab label="ACC" {...a11yProps(5)} />
                 </Tabs>
             </Grid>
-            <Grid item xs={10}>
+            <Grid item md={10} xs={12}>
                 <TabPanel value={value} index={0}>
-                    <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
+                    <Grid container spacing={2} direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
                         {productsTemplate('ALL')}
                     </Grid>
                 </TabPanel>
                 <TabPanel value={value} index={1}>
-                    <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
+                    <Grid container spacing={2} direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
                         {productsTemplate('OUTER')}
                     </Grid>
                 </TabPanel>
                 <TabPanel value={value} index={2}>
-                    <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
+                    <Grid container spacing={2} direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
                         {productsTemplate('TOP')}
                     </Grid>
                 </TabPanel>
                 <TabPanel value={value} index={3}>
-                    <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
+                    <Grid container spacing={2} direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
                         {productsTemplate('BOTTOM')}
                     </Grid>
                 </TabPanel>
                 <TabPanel value={value} index={4}>
-                    <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
+                    <Grid container spacing={2} direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
                         {productsTemplate('BAG & SHOES')}
                     </Grid>
                 </TabPanel>
                 <TabPanel value={value} index={5}>
-                    <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
+                    <Grid container spacing={2} direction="row" justify="center" alignItems="center" style={{marginTop: 30}}>
                         {productsTemplate('ACC')}
                     </Grid>
                 </TabPanel>
@@ -115,4 +125,4 @@ const Products = () => {
     );
 };
 
-export default Products;
+export default withWidth()(Products);
