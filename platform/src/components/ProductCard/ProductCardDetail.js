753e4880bbce1027299ec4631ec4bf28e0dcd035
R _ R <exit19093@gmail.com>
2021-09-09 16:49:32 +0900

    Fix: add to bag button size 변경

--- platform/src/components/ProductCard/ProductCardDetail.js
+++ platform/src/components/ProductCard/ProductCardDetail.js
@@ -1,16 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import './ProductCardDetail.css';
 import {useDispatch, useSelector, connect} from "react-redux";
-import { addProductToCart } from "../../modules/actions/shoppingCartAction";
+import {addProductToCart} from "../../modules/actions/shoppingCartAction";
 import allAction from "../../modules/actions";
 import {Button, Grid, List, ListItem, ListItemText, Typography} from "@material-ui/core";
 import Footer from "../Footer/Footer";
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
 const ProductCardDetail = (props) => {
     const params = props.match.params.path;
     const dispatch = useDispatch();
     const {data, loading, error} = useSelector(state => state.products.product[params]) || {
-        loading: false, data: null, error: null};
+        loading: false, data: null, error: null
+    };
     const [secondary, setSecondary] = useState(false);
 
     useEffect(() => {
@@ -23,51 +25,66 @@ const ProductCardDetail = (props) => {
 
     return (
         <div>
-            <div style={{backgroundColor: 'rgb(25,25,25)', height: 120}}></div>
-            <Grid container direction="row" justify="center" alignItems="center" style={{marginTop: 30, textAlign: "center"}}>
+            <div style={{backgroundColor: 'rgb(25, 25, 25)', height: window.innerWidth <= 850 ? 55 : 120}}></div>
+            <Grid container direction="row" justify="center" alignItems="center"
+                  style={{marginTop: 30, textAlign: "center"}}>
                 {data ? (
                     <Grid container item xs={12} direction="row" alignItems="center">
-                        <Grid container item xs={8} direction="row" style={{paddingLeft: '5%', paddingRight: '3%'}}>
-                            <Grid item xs={6} className="padding-right">
+                        <Grid item container md={8} xs={12} direction="row" spacing={1} style={{marginBottom: 30}}>
+                            <Grid item xs={6}>
                                 <img src={data.mainImage} width="100%" alt={data.title}/>
                             </Grid>
-                            <Grid item xs={6} className="padding-left">
+                            <Grid item xs={6}>
                                 <img src={data.subImage} width="100%" alt={data.title}/>
                             </Grid>
-                            <Grid item xs={4} className="padding-right">
+                            <Grid item xs={4}>
                                 <img src={data.subImage2} width="100%" alt={data.title}/>
                             </Grid>
-                            <Grid item xs={4} className="padding">
+                            <Grid item xs={4}>
                                 <img src={data.subImage3} width="100%" alt={data.title}/>
                             </Grid>
-                            <Grid item xs={4} className="padding-left">
+                            <Grid item xs={4}>
                                 <img src={data.subImage4} width="100%" alt={data.title}/>
                             </Grid>
                         </Grid>
-                        <Grid item xs={4} className="paddingRight">
-                            <Typography variant="h5">{data.title}</Typography>
-                            <Typography variant="subtitle1">{data.subtitle}</Typography>
+                        <Grid item container md={4} xs={12}>
+                            <Grid item xs={12}>
+                                <Typography variant="h5">{data.title}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography paragraph variant="subtitle1">{data.subtitle}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography variant="h6">$ {data.price.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}<span
+                                    className="price-tax"> (included tax) </span></Typography>
+                            </Grid>
                             <br/><br/>
-                            <Typography variant="h6">$ {data.price.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}<span className="price-tax"> (included tax) </span></Typography>
-                            <br/><br/>
-                            <List className="product-info">
+                            <Grid item xs={12}>
                                 <ListItem style={{textAlign: 'center'}}>
                                     <ListItemText primary="Color" secondary={secondary ? '' : data.color}/>
                                     <ListItemText primary="Size" secondary={secondary ? '' : data.size}/>
                                 </ListItem>
+                            </Grid>
+                            <Grid item xs={12}>
                                 <ListItem style={{textAlign: 'center'}}>
                                     <ListItemText primary="Material" secondary={secondary ? '' : data.material}/>
                                     <ListItemText primary="StyleCode" secondary={secondary ? '' : data.styleCode}/>
                                 </ListItem>
-                            </List>
-                            <br/><br/>
-                            <Typography variant="body1">{data.detail}</Typography>
-                            <br/><br/>
-                            <Button variant="outlined" color="secondary"
-                                    onClick={() => {addProduct(data); alert("Item Successfully added to your cart")}}
-                                    className="cart-button">Add cart</Button>
+                            </Grid>
+                            <Grid item xs={12} style={{marginTop: 15}}>
+                                <Typography variant="body1" paragraph>{data.detail}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button variant="outlined" color="secondary" className="cart-button" size="large"
+                                        onClick={() => {
+                                            addProduct(data);
+                                            alert("Item Successfully added to your cart")
+                                        }}>
+                                    <ShoppingCartOutlinedIcon/> Add to Bag
+                                </Button>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} style={{marginTop: '5%'}}>
+                        <Grid item xs={12} style={{marginTop: 30}}>
                             <Footer/>
                         </Grid>
                     </Grid>
