a81fe98508493e60879f7ee0ed6b3675a0e12bb5
R _ R <exit19093@gmail.com>
2021-09-09 16:51:31 +0900

    Feat: Footer component responsible UI 업데이트

--- platform/src/components/Footer/Footer.js
+++ platform/src/components/Footer/Footer.js
@@ -9,49 +9,63 @@ function ListItemLink(props) {
 
 const Footer = () => {
     return (
-        <div class="site-footer">
+        <div className="site-footer">
             <Grid container style={{width: '80%', margin: "auto"}}>
                 <Grid container direction="row">
-                    <Grid md={7} sm={12}>
+                    <Grid item md={7} sm={12} xs={12}>
                         <Typography variant="h6" align="left">The Salt</Typography>
-                        <Typography variant="h6" align="left">High Quality Select Shop</Typography>
+                        <Typography variant="h6" paragraph align="left">High Quality Select Shop</Typography>
                     </Grid>
-                    <Grid md={2} sm={6} xs={6}>
+                    <Grid item md={2} sm={6} xs={6}>
                         <Typography variant="h6" align="left">Quick Links</Typography>
                         <List>
                             <ListItemLink href="/home" style={{padding: 0}}><ListItemText
                                 primary="Home"/></ListItemLink>
                             <ListItemLink href="/product" style={{padding: 0}}><ListItemText
                                 primary="Products"/></ListItemLink>
+                            <ListItemLink href="/product" style={{padding: 0}}><ListItemText
+                                primary="Products"/></ListItemLink>
+                            <ListItemLink href="/product" style={{padding: 0}}><ListItemText
+                                primary="Products"/></ListItemLink>
                         </List>
                     </Grid>
 
-                    <Grid md={3} sm={6} xs={6}>
-                        <Typography variant="h6" align="right">Contact Us</Typography>
+                    <Grid item md={3} sm={6} xs={6}>
+                        <Typography variant="h6">Contact Us</Typography>
                         <List>
-                            <ListItem button style={{padding: 0, textAlign: 'right'}}><ListItemText
+                            <ListItem button style={{padding: 0, textAlign: 'center'}}><ListItemText
                                 primary="010-2895-1359"/></ListItem>
                         </List>
                     </Grid>
                 </Grid>
             </Grid>
             <Divider variant="middle"
-                     style={{width: '82%', margin: "auto", marginTop: 10, height: 1, backgroundColor: '#bbb'}}/>
-            <Grid container style={{width: '80%', margin: "auto"}} alignItems="center">
-                <Grid item md={8}>
-                    <Typography variant="body1" align="left">Copyright &copy; 2021 All Rights Reserved by <a
+                     style={{
+                         width: '82%',
+                         margin: "auto",
+                         marginTop: 20,
+                         marginBottom: 20,
+                         height: 1,
+                         backgroundColor: '#bbb'
+                     }}/>
+            <Grid container style={{width: '80%', margin: "auto"}} alignItems="center" justify="center">
+                <Grid item xs={12}>
+                    <Typography variant="body1">Copyright &copy; 2021 All Rights Reserved by <a
                         href="#">CMM</a>.</Typography>
                 </Grid>
-                <Grid item md={4} sm={6} xs={12} alignItems="center" justify="center">
+                <Grid item xs={12}>
                     <ul className="social-icons">
-                        <li><a className="facebook" href="https://www.facebook.com"><Facebook className="fa fa-facebook"
-                                                                                              style={{height: '1.5rem !important'}}/></a>
+                        <li><a className="facebook" href="https://www.facebook.com">
+                            <Facebook className="fa fa-facebook"/></a>
                         </li>
-                        <li><a className="instagram" href="https://www.instagram.com"><Instagram
-                            className="fa-instagram"/></a></li>
-                        <li><a className="twitter" href="https://twitter.com"><Twitter className="fa-twitter"/></a></li>
-                        <li><a className="linkedin" href="https://kr.linkedin.com"><LinkedIn
-                            className="fa-linkedin"></LinkedIn></a></li>
+                        <li><a className="instagram" href="https://www.instagram.com">
+                            <Instagram className="fa-instagram"/></a>
+                        </li>
+                        <li><a className="twitter" href="https://twitter.com">
+                            <Twitter className="fa-twitter"/></a></li>
+                        <li><a className="linkedin" href="https://kr.linkedin.com">
+                            <LinkedIn className="fa-linkedin"></LinkedIn></a>
+                        </li>
                     </ul>
                 </Grid>
             </Grid>
