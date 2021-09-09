a679b97d13ea7eed972d60a6caedf80e0cc18130
R _ R <exit19093@gmail.com>
2021-09-09 16:50:38 +0900

    Feat: Screen component responsible UI 적용

--- platform/src/components/Screen/Screen.js
+++ platform/src/components/Screen/Screen.js
@@ -2,15 +2,9 @@ import React from "react";
 import './Screen.css'
 import Fullpage, {FullPageSections, FullpageSection, FullpageNavigation} from '@ap.cx/react-fullpage'
 import ShowMoreButton from "../ShowMoreButton/ShowMoreButton";
-import {createTheme, responsiveFontSizes, ThemeProvider} from '@material-ui/core/styles';
-
-
 import {Grid, Typography} from "@material-ui/core"
 import video from '../../factory/v1.mp4'
 import min from '../../factory/images/min.png'
-
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
 
 export function Screen() {
 
@@ -26,13 +20,11 @@ export function Screen() {
                         <Grid item md={6} xs={2}></Grid>
                         <Grid item md={6} xs={10}>
                             <div style={{paddingRight: '15%'}}>
-                                <ThemeProvider theme={theme}>
-                                    <Typography variant="h1" className="title">THE SALT</Typography>
-                                    <Typography variant="h3" className="subtitle">High Quality Select Shop</Typography>
-                                    <Typography variant="h3" className="buttonText">
-                                        <ShowMoreButton label="show more"/>
-                                    </Typography>
-                                </ThemeProvider>
+                                <Typography variant="h1" className="title">THE SALT</Typography>
+                                <Typography variant="h3" className="subtitle" paragraph>High Quality Select Shop</Typography>
+                                <Typography variant="h3" className="buttonText">
+                                    <ShowMoreButton label="show more"/>
+                                </Typography>
                             </div>
                         </Grid>
                     </Grid>
@@ -41,17 +33,15 @@ export function Screen() {
                     <Grid container direction="row" justify="center" alignItems="center" style={{textAlign: "center"}}>
                         <Grid item xs={12}>
                             <div style={{height: 120}}></div>
-                            <ThemeProvider theme={theme}>
-                                <Typography variant="h3" className="introduce">CREATOR</Typography>
-                                <img src={min} width='280px' className="avatar"/>
-                                <Typography variant="h4" className="smallCaps">minho choi</Typography>
-                                <Typography variant="h5" className="smallCaps">computer science</Typography>
-                                <Typography variant="h5" className="info">
-                                    <a href='https://github.com/mminhou/cntex'>https://github.com/mminhou/cntex</a>
-                                </Typography>
-                                <Typography variant="h5" className="info">exit19093@gmail.com</Typography>
-                                <Typography variant="h5" className="info">010-2895-1359</Typography>
-                            </ThemeProvider>
+                            <Typography variant="h3" className="introduce">CREATOR</Typography>
+                            <img src={min} width='280px' className="avatar"/>
+                            <Typography variant="h4" className="smallCaps">minho choi</Typography>
+                            <Typography variant="h5" className="smallCaps">computer science</Typography>
+                            <Typography variant="h5" className="info">
+                                <a href='https://github.com/mminhou/cntex'>https://github.com/mminhou/cntex</a>
+                            </Typography>
+                            <Typography variant="h5" className="info">exit19093@gmail.com</Typography>
+                            <Typography variant="h5" className="info">010-2895-1359</Typography>
                         </Grid>
                     </Grid>
                 </FullpageSection>
