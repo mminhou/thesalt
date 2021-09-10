01d1c3809d981789115676987be277b936528121
R _ R <exit19093@gmail.com>
2021-09-11 03:44:13 +0900

    Feat: 요청 Response로 Token과 user정보 return

--- backend/override/jwt.py
+++ backend/override/jwt.py
@@ -1,9 +1,9 @@
 from accounts.serializers import UserSerializer
 
+# /api-token-auth -> return token 만 반환됨 이때, user 정보도 반환하기 위해서 함수 정의
 def jwt_response_payload_handler(token, user=None, request=None):
-    user = UserSerializer(user, context={'request': request}).data
     return {
         'token': token,
-        'userid': user['id'],
-        'username':user['username']
+        'user': UserSerializer(user, context={'request': request}).data
     }
+
